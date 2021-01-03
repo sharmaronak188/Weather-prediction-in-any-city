2db0361c65a0eb560be407121c69e9ecfa072cfa
sharmaronak188 <sharmaronak188@gmail.com>
2021-01-03 18:42:53 +0530

    Comment added in app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// A simple Weather prediction application for any city around the world.
+
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
